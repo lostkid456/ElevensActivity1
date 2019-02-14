8df718a50f31b2306365a5894e962c347efe238e
BT_1N3_32 <1q>
2019-02-14 14:35:47 -0500

    LoL

--- src/CardTester.java
+++ src/CardTester.java
@@ -2,4 +2,12 @@
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+    public static void main(String[] args){
+        Card card1= new Card("9","Spades",9);
+        Card card2=new Card("A","Spades",1);
+        Card card3=new Card("Joker","None",100);
+
+        System.out.println(card1.matches(card2));
+        System.out.println(card2.matches(card3));
+    }
 }
